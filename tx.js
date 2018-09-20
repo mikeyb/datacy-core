a275e9881eb640dbe4658c8a36ee2c6d4d5b22f2
kalebjordan1 <kalebjordanwilson@gmail.com>
2018-09-19 22:22:45 -0400

    Added tx.js

---
+++ tx.js
@@ -0,0 +1,29 @@
+var moment = require('moment')
+var crypto = require('crypto')
+var express = require('express')
+var Cryptr = require('cryptr')
+var _ = require('underscore')
+var Math = require('math.js')
+
+// copy of a transaction that is sent to the sender
+class TxIn {
+  constructor(data, from, to, id){
+    this.id = id
+    this.data = data
+    this.timestamp = moment()format('x')
+    this.from = from
+    this.to = to
+  }
+}
+
+// copy of a transaction that is sent to the new owner
+class TxOut {
+  constructor(data, from, to, id) {
+    this.id = id
+    this.data = data
+    this.timestamp = timestamp
+    this.from = from
+    this.to = to
+
+  }
+}
